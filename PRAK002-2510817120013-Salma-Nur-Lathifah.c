3741d76b3b9220708ca7bbb8dc1f54926ab4da75
Salma Nur Lathifah <25salma.nurlathifah@gmail.com>
2025-09-30 00:21:29 +0800

    Upload C File

---
+++ PRAK002-2510817120013-Salma-Nur-Lathifah.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){
+    printf("Selamat Pagi, Salma \n");
+    printf("Selamat Siang, Salma \n");
+    printf("Selamat Malam, Salma");
+    return 0;
+}
---
+++ PRAK004-2510817120013-Salma-Nur-Lathifah.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    printf("############################## \n\n");
+    printf("#                            # \n\n");
+    printf("#     Salma Nur Lathifah     # \n\n");
+    printf("#        2510817120013       # \n\n");
+    printf("#                            # \n\n");
+    printf("##############################");
+    return 0;
+}
